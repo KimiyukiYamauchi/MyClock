5e5e269d0233c2253d53e7a1f5e4d49955ac76b0
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2012-09-20 10:44:47 +0900

    9/20 10:44 完成

--- src/sample/application/clock/MyClockActivity.java
+++ src/sample/application/clock/MyClockActivity.java
@@ -1,20 +1,40 @@
 package sample.application.clock;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MyClockActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private int counter = 1;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	public void onClick(View v) {
+
+		Log.d(ACTIVITY_SERVICE, "onClickが呼ばれました");
+
+		View layout = findViewById(R.id.layout);
+
+		if (counter == 1) {
+			layout.setBackgroundResource(R.drawable.img2);
+			counter = 2;
+		} else {
+			layout.setBackgroundResource(R.drawable.img1);
+			counter = 1;
+		}
+
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
